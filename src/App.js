5ff7ea8a22b5b78faf290a3b7d359c8e915670cb
Catherine Muya <129610930+Katemuya@users.noreply.github.com>
2023-05-25 14:42:16 +0200

    add state activity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
+import { uid } from "uid";
 
 export default function App() {
-  return <Form></Form>;
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(newData) {
+    setActivities([...activities, { ...newData, id: uid() }]);
+  }
+  return <Form onAddActivity={handleAddActivity}></Form>;
 }
